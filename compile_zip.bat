d680cfdaf3e530814565af439419f8c428416c53
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-08-01 21:59:20 -0500

    Include requirements.txt

--- compile_zip.bat
+++ compile_zip.bat
@@ -8,12 +8,13 @@ set FILE1=albumworks.py
 set FILE2=albumworks.bat
 set FILE3=README.md
 set FILE4=notes.txt
+set FILE5=requirements.txt
 
 :: set the output zip file name
 set ZIPNAME=AlbumWorks.zip
 
 :: create the zip archive with the files using tar
-tar -a -c -f "%ZIPNAME%" "%FILE1%" "%FILE2%" "%FILE3%" "%FILE4%"
+tar -a -c -f "%ZIPNAME%" "%FILE1%" "%FILE2%" "%FILE3%" "%FILE4%" "%FILE5%"
 
 if %ERRORLEVEL% neq 0 (
     echo Failed to create archive.
