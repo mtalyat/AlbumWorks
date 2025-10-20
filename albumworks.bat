a1da951a74d86cc35bb8a5557981ab5086db7322
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-10-19 23:50:12 -0500

    Update albumworks.bat

--- albumworks.bat
+++ albumworks.bat
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
+@echo off
+echo Installing required packages...
+pip install -r %~dp0requirements.txt 2>&1 >nul
+if not %ERRORLEVEL%==0 (
+    echo Failed to install required packages.
+    pause
+    exit /b %ERRORLEVEL%
+)
+cls
 python %~dp0albumworks.py %*
